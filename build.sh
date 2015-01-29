def9b37f0e6cc4060b85cac352b6dc2e22d649c8
Sebastian Krysmanski <noreply@manski.net>
2015-01-29 19:18:54 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SOURCES="assets font *.css *.php readme.txt"
+DEST_DIR="dist"
+
+rm -rf $DEST_DIR
+
+mkdir $DEST_DIR
+
+rsync --recursive --human-readable --times --delete $SOURCES $DEST_DIR/
