f114065c9808f4f905fba0f5ccc57082dcedb416
Sebastian Krysmanski <noreply@manski.net>
2015-01-29 19:47:11 +0100

    The build script now also creates a zip file of the plugin

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 SOURCES="font *.css *.php readme.txt"
-DEST_DIR="dist"
+DEST_DIR="dist/font-emoticons"
 
 rm -rf $DEST_DIR
 
-mkdir $DEST_DIR
+mkdir -p $DEST_DIR
 
 rsync --recursive --human-readable --times --delete $SOURCES $DEST_DIR/
+
+zip -9 -r dist/font-emoticons.zip $DEST_DIR/
