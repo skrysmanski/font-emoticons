cf45b1092df1dcded77fc67b0d1bc5aa16059749
Sebastian Krysmanski <noreply@manski.net>
2015-01-30 07:49:25 +0100

    Improved build script

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-SOURCES="font *.css *.php readme.txt"
-DEST_DIR="dist/font-emoticons"
+DIST_DIR="dist"
 
-rm -rf $DEST_DIR
+SOURCES="font *.css *.php readme.txt"
+DEST_DIR="$DIST_DIR/trunk"
 
 mkdir -p $DEST_DIR
+rsync --recursive --human-readable --times --delete --exclude=.svn $SOURCES $DEST_DIR/
 
-rsync --recursive --human-readable --times --delete $SOURCES $DEST_DIR/
+mkdir -p "$DIST_DIR/assets"
+rsync --recursive --human-readable --times --delete --exclude=.svn "assets/" "$DIST_DIR/assets"
 
-zip -9 -r dist/font-emoticons.zip $DEST_DIR/
+ZIP_DIR="$DIST_DIR/font-emoticons"
+ZIP_FILE="font-emoticons.zip"
+mkdir -p $ZIP_DIR
+rsync --recursive --human-readable --times --delete $SOURCES $ZIP_DIR/
+cd $DIST_DIR
+rm $ZIP_FILE || true
+zip -9 -r $ZIP_FILE "font-emoticons"
+cd -
+rm -rf "$ZIP_DIR"
---
+++ dist/readme.txt
@@ -0,0 +1,1 @@
+Check out "trunk" and "assets" from the Wordpress repository in this folder.
