3df8ec6599cc141583f6c2e19b8cd1d4a0affcbb
Sebastian Krysmanski <noreply@manski.net>
2019-01-13 08:06:09 +0100

    Extracted “escape and join” code in FontEmoticonInfo into its own method

--- src/FontEmoticonInfo.php
+++ src/FontEmoticonInfo.php
@@ -19,9 +19,37 @@ class FontEmoticonInfo
      */
     private $m_htmlCode;
 
+    /**
+     * FontEmoticonInfo constructor.
+     *
+     * @param string    $name
+     * @param string[]  $textRepresentations text representations of this smiley/emoticon
+     */
     public function __construct($name, $textRepresentations)
     {
-        $this->m_regex = '';
+        $joinedRegex = self::escapeAndJoinTextRepresentations($textRepresentations);
+
+        // NOTE: We need to use lookahead and lookbehind here (instead of capturing the leading and
+        //   trailing whitespace) so that multiple consecutive emoticons are detected correctly (see issue #5).
+        // NOTE 2: All HTML tags have been escaped at this point (with "DELIM_CHARS" being the marker).
+        $this->m_regex = '/(?<=\s|^|' . FontEmoticonsPlugin::DELIM_CHARS . ')'
+                       . '(?:' . $joinedRegex . ')'
+                       . '(?=\s|$|' . FontEmoticonsPlugin::DELIM_CHARS . ')/U';
+
+        $this->m_htmlCode = '<span class="' . self::EMOTS_BASE_CLASS_NAME . $name . '"></span>';
+    }
+
+    /**
+     * Escapes each text representation with "preg_quote()" and then joins all
+     * of them together with "|" - resulting in a valid regex.
+     *
+     * @param string[] $textRepresentations text representations of this smiley/emoticon
+     *
+     * @return string the resulting regex
+     */
+    private static function escapeAndJoinTextRepresentations($textRepresentations)
+    {
+        $regex = '';
         $is_first = true;
 
         foreach ($textRepresentations as $smiley)
@@ -32,20 +60,13 @@ class FontEmoticonInfo
             }
             else
             {
-                $this->m_regex .= '|';
+                $regex .= '|';
             }
 
-            $this->m_regex .= preg_quote($smiley, '/');
+            $regex .= preg_quote($smiley, '/');
         }
 
-        // NOTE: We need to use lookahead and lookbehind here (instead of capturing the leading and
-        //   trailing whitespace) so that multiple consecutive emoticons are detected correctly (see issue #5).
-        // NOTE 2: All HTML tags have been escaped at this point (with "DELIM_CHARS" being the marker).
-        $this->m_regex = '/(?<=\s|^|' . FontEmoticonsPlugin::DELIM_CHARS . ')'
-                       . '(?:' . $this->m_regex . ')'
-                       . '(?=\s|$|' . FontEmoticonsPlugin::DELIM_CHARS . ')/U';
-
-        $this->m_htmlCode = '<span class="' . self::EMOTS_BASE_CLASS_NAME . $name . '"></span>';
+        return $regex;
     }
 
     public function replaceTextEmots($postText)
